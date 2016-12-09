0e1bb6b25c35fe2e05eecff0db0fdbac63af297b
worker <worker@wworker.com>
2016-12-09 06:59:08 +0000

    OK

--- install-jdk.sh
+++ install-jdk.sh
@@ -4,7 +4,7 @@ declare -r VERSION=${0:-"8u112"}
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz
 
 mkdir /opt/jdk
-
+cd /opt/jdk
 tar -zxf jdk-8u112-linux-x64.tar.gz
 
 update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_112/bin/java 100
@@ -12,9 +12,11 @@ update-laternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_112/bin/jav
 
 echo "Verify Java version:"
 update-alternatives --display java
-update-laternatives --display javac
+update-alternatives --display javac
 java -version
 
+echo "export JAVA_HOME=/opt/jdk/jdk1.8.0_112"
 echo "export JAVA_HOME=/opt/jdk/jdk1.8.0_112" >> ~/.profile
+echo "export PATH=\"$PATH:$JAVA_HOME/bin\""
 echo "export PATH=\"$PATH:$JAVA_HOME/bin\"" >> ~/.profile
 
